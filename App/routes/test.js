c5e82f45273b642b92c60efb716019b235e2e1c4
lll-jy <E0376997@u.nus.edu>
2020-10-26 11:20:10 +0800

    Test for database connection

---
+++ App/routes/test.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+require('dotenv').config({path: __dirname + '/../.env'});
+
+const { Pool } = require('pg')
+
+const pool = new Pool({connectionString:process.env.DATABASE_URL})
+
+/* SQL Query */
+var sql_query = 'SELECT * FROM AppUsers';
+
+router.get('/', function(req, res, next) {
+	pool.query(sql_query, (err, data) => {
+		console.log(err);
+		console.log(data);
+		res.render('test', { title: 'Database Connect', data: data.rows });
+	});
+});
+
+module.exports = router;
