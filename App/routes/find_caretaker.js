32bfc03ea7c9bed5da0d07273c4f8f6f8e81a637
lll-jy <E0376997@u.nus.edu>
2020-10-27 14:21:19 +0800

    Check valid format of dates

--- App/routes/find_caretaker.js
+++ App/routes/find_caretaker.js
@@ -14,12 +14,18 @@ var all_caretaker_query = 'SELECT ct.userid AS userid, name, rating, daily_price
 	'           WHEN EXISTS(SELECT 1 FROM FullTimeCareTakers fct WHERE fct.userid=ct.userid) THEN \'Full-time\'\n' +
 	'           ELSE \'Part-Time\'\n' +
 	'           END\n' +
-	'                 AS category\n' +
+	'       AS category\n' +
 	'FROM ((SELECT userid, rating\n' +
 	'      FROM CareTakers\n' +
 	'      WHERE NOT EXISTS(SELECT 1 FROM CannotTakeCare WHERE ct_id=userid AND category=$1)) ct\n' +
 	'    NATURAL JOIN (SELECT userid, name FROM Users) us)\n' +
-	'    LEFT JOIN (SELECT ct_id, daily_price FROM CanTakeCare) ctc ON ct.userid=ctc.ct_id;' //all caretakers can take care of this category of pet
+	'    LEFT JOIN (SELECT ct_id, daily_price FROM CanTakeCare) ctc ON ct.userid=ctc.ct_id\n' +
+	'WHERE EXISTS(\n' +
+	'    SELECT 1 FROM PeriodsAvailable pa\n' +
+	'    WHERE pa.ct_id=ct.userid\n' +
+	'      AND s_date <= $2\n' +
+	'      AND e_date >= $3\n' +
+	'          );' //all caretakers can take care of this category of pet at this period of time
 /*
 all_caretaker_query =
 SELECT ct.userid AS userid, name, rating, daily_price,
@@ -32,7 +38,13 @@ FROM ((SELECT userid, rating
       FROM CareTakers
       WHERE NOT EXISTS(SELECT 1 FROM CannotTakeCare WHERE ct_id=userid AND category=$1)) ct
     NATURAL JOIN (SELECT userid, name FROM Users) us)
-    LEFT JOIN (SELECT ct_id, daily_price FROM CanTakeCare) ctc ON ct.userid=ctc.ct_id;
+    LEFT JOIN (SELECT ct_id, daily_price FROM CanTakeCare) ctc ON ct.userid=ctc.ct_id
+WHERE EXISTS(
+    SELECT 1 FROM PeriodsAvailable pa
+    WHERE pa.ct_id=ct.userid
+      AND s_date <= $2
+      AND e_date >= $3
+          );
  */
 
 /* Data */
@@ -44,16 +56,35 @@ var category;
 var requirements;
 var petOwners;
 var careTakers;
-var s_date = new Date();
+var s_date;
 var e_date;
 
 /* Util */
 var getString = (date) => date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+var compareDates = (d1, d2) => {
+	if (d1.getFullYear() > d2.getFullYear()) {
+		return 1;
+	} else if (d1.getFullYear() < d2.getFullYear()) {
+		return -1;
+	} else if (d1.getMonth() > d2.getMonth()) {
+		return 1;
+	} else if (d1.getMonth() < d2.getMonth()) {
+		return -1;
+	} else if (d1.getDate() > d2.getDate()) {
+		return 1;
+	} else if (d1.getDate() < d2.getDate()) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
 
 /* Err msg */
 var connectionSuccess;
 var isPetOwner;
 var isValidPet;
+var sDateErr;
+var eDateErr;
 
 // GET
 router.get('/:userid/:petid', function(req, res, next) {
@@ -80,9 +111,10 @@ router.get('/:userid/:petid', function(req, res, next) {
 				petName = pet[0].name;
 				category = pet[0].category;
 				requirements = pet[0].requirements;
+				s_date = new Date();
 				e_date = new Date(s_date);
 				e_date.setDate(s_date.getDate() + 7);
-				pool.query(all_caretaker_query, [category], (err, data) => {
+				pool.query(all_caretaker_query, [category, getString(s_date), getString(e_date)], (err, data) => {
 					careTakers = data.rows;
 				})
 				res.render('find_caretaker', {
@@ -107,6 +139,14 @@ router.get('/:userid/:petid', function(req, res, next) {
 // POST
 router.post('/:userid/:petid', function(req, res, next) {
 	petid = req.params.petid;
+	s_date = new Date(req.body.s_date.trim());
+	e_date = new Date(req.body.e_date.trim());
+	if (s_date.toString() === 'Invalid Date') {
+		sDateErr = "The start date format is not a valid date format.";
+	}
+	if (e_date.toString() === 'Invalid Date') {
+		eDateErr = "The end date format is not a valid date format.";
+	}
 });
 
 module.exports = router;
