93f559fdb54ecfe37d0c8620d04e259081dc816f
lll-jy <E0376997@u.nus.edu>
2020-10-28 11:50:55 +0800

    Access data to render

--- App/routes/new_transaction.js
+++ App/routes/new_transaction.js
@@ -6,9 +6,10 @@ const { Pool } = require('pg')
 const pool = new Pool({connectionString:process.env.DATABASE_URL})
 
 /* SQL Query */
-var all_petowner_query = 'SELECT userid FROM PetOwners';
-var petowner_exist_query = 'SELECT 1 FROM PetOwners WHERE userid=$1'
+var all_petowner_query = 'SELECT 1 FROM PetOwners';
+var petowner_exist_query = 'SELECT 1 FROM PetOwners WHERE userid=$1';
 var pet_exist_query = 'SELECT * FROM Pets WHERE petid=$1 AND owner=$2';
+var request_exist_query = 'SELECT s_date, e_date, transfer_type, payment_type FROM Requests WHERE pet_id=$1 AND s_date=$2';
 var conflicting_query = 'SELECT s_date, e_date FROM Requests WHERE pet_id=$1 AND are_conflicting_periods(s_date, e_date, $2, $3)';
 var submit_request_query = 'INSERT INTO Requests VALUES ($1, $2, $4, $5, $3)'
 
@@ -18,8 +19,6 @@ var petid;
 var pet;
 var petName;
 var category;
-var requirements;
-var petOwners;
 var s_date;
 var e_date;
 var transfer_type;
@@ -54,7 +53,6 @@ var refreshPage = (res) => {
 		title: 'Find Care Taker for ' + petName,
 		petid: petid,
 		category: category,
-		requirements: requirements,
 		s_date: getString(s_date),
 		e_date: getString(e_date),
 		sDateErr: sDateErr,
@@ -74,12 +72,11 @@ var dateConflictErr = "";
 router.get('/:userid/:petid/:s_date', function(req, res, next) {
 	userid = req.params.userid; //TODO: May need to update with session user id
 	petid = req.params.petid;
-	s_date = req.params.s_date;
+	s_date = new Date(req.params.s_date);
 	pool.query(all_petowner_query, (err, data) => {
 		if (err !== undefined) {
 			res.render('connection_error');
 		} else {
-			petOwners = data.rows;
 			pool.query(petowner_exist_query, [userid], (err, data) => {
 				if (data.rows.length > 0) {
 					pool.query(pet_exist_query, [petid, userid], (err, data) => {
@@ -87,8 +84,18 @@ router.get('/:userid/:petid/:s_date', function(req, res, next) {
 							pet = data.rows;
 							petName = pet[0].name;
 							category = pet[0].category;
-							requirements = pet[0].requirements;
-							refreshPage(res);
+							pool.query(request_exist_query, [petid, getString(s_date)], (err, data) => {
+								if (data.rows.length > 0) {
+									var request = data.rows[0];
+									e_date = request.e_date;
+									transfer_type = request.transfer_type;
+									payment_method = request.payment_method;
+									console.log('success');
+									refreshPage(res);
+								} else {
+									res.render('not_found_error', {component: 'request'});
+								}
+							})
 						} else {
 							res.render('not_found_error', {component: 'petid'});
 						}
